1cc9d1d0fda223897e57166ee188ece866b72876
Arthur Guigand <aguigand@umanit.fr>
2014-10-21 14:31:10 +0200

    Intégration du data-transformer

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Form\UserType;
+use AppBundle\Entity\User;
 
 /**
  * Controller des utilisateurs
@@ -21,9 +23,28 @@ class UserController extends Controller
      * 
      * @Route("/sign-in")
      * @Template("AppBundle:User:create.html.twig")
+     * @param Request $request
+     * @return array
      */
     public function signInAction(Request $request)
     {
+        $user = new User();
+        $form = $this->createForm(new UserType(), $user, array(
+            'em' => $this->getDoctrine()->getManager()
+        ));
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            // Enregistrement en base
+            $this->getDoctrine()->getManager()->persist($user);
+            $this->getDoctrine()->getManager()->flush();
+        }
+
+        return array(
+            'form' => $form->createView(),
+            'valid' => $form->isValid()
+        );
     }
 
 }
